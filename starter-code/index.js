4467e8d3b7915861eb10c62051f34d570cd50fb2
imadomar64 <imadomar64@gmail.com>
2025-01-16 07:56:17 +0100

    Update index.js

--- starter-code/index.js
+++ starter-code/index.js
@@ -5,7 +5,7 @@ const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello aws cloud demos!!ver222');
+  res.end('Hello aws cloud demos!!ver25');
 });
 
 server.listen(port,() => {
